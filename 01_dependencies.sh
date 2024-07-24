f379d68bfb102f77d84676e38b50ab0998fd14b4
jGDarkness <jeremy.g.davenport@gmail.com>
2024-07-24 17:45:01 -0400

    revised for test results on 07242024

--- 01_dependencies.sh
+++ 01_dependencies.sh
@@ -1,35 +1,41 @@
-#! /bin/bash
+#!/bin/bash
 
-# Baseline Dependencies
+# Check if the script is running from its own directory
+if [ ! -f "$(basename "$0")" ]; then
+    echo "Error: This script must be run from the directory where it is stored."
+    echo "Please cd to the script's directory and try again."
+    exit 1
+fi
 
-## Update, Upgrade, and Dist-upgrade
+# Check if the script is running with sudo privileges
+if [ "$EUID" -ne 0 ]; then
+    # Check if the user is in the sudo group
+    if groups $USER | grep &>/dev/null '\bsudo\b'; then
+        echo "You are in the sudo group, but didn't run this script with sudo privileges."
+        echo "Please run the script again using: sudo $0"
+    else
+        echo "You are not in the sudo group."
+        echo "Please add your username to the sudoers group and try again."
+        echo "You can do this by running: sudo usermod -aG sudo $USER"
+    fi
+    exit 1
+fi
+
+## Update, Upgrade, and Autoremove
 sudo apt update
 sudo apt upgrade -y
-sudo apt dist-upgrade
-sudo apt autoremove
+sudo apt autoremove -y
 
 # Make .dotfile directories
-mkdir -p $HOME/.config
-
+sudo mkdir -p $HOME/.config
 sudo truncate -s 0 /etc/apt/sources.list
-
 sudo cp sources.list /etc/apt/sources.list.d/sources.list
-
-sudo get update
-
+sudo apt update
 sudo apt upgrade -y
-
 sudo apt install wget gpg apt-transport-https flatpak gnome-software-plugin-flatpak timeshift linux-headers-amd64 xdotool imagemagick chrome-gnome-shell gnome-shell-extension-manager -y
-
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-
 sudo apt install nvidia-driver firmware-misc-nonfree -y
 
-# Comment out the lines with (: <<'END_COMMENT') and (END_COMMENT) to install the latest kernel, and ensure it's updated
-# when the rest of the system updates. Replace them before and after the block without the () to install the stable
-# kernel.
-
-#: <<'END_COMMENT'
 sudo apt -t bookworm-backports install linux-image-amd64 linux-headers-amd64 -y
 sudo tee -a /etc/apt/preferences << EOF
 Package: linux-image-*
@@ -40,10 +46,5 @@ Package: linux-headers-*
 Pin: release a=bookworm-backports
 Pin-Priority: 500
 EOF
-#END_COMMENT
 
-
-# Reboot
-sudo reboot
-## Reboot
 sudo reboot
--- 02_core_apps.sh
+++ 02_core_apps.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
-# Check to see if the script is being run with sudo permissions.
+# Check if the script is running from its own directory
+if [ ! -f "$(basename "$0")" ]; then
+    echo "Error: This script must be run from the directory where it is stored."
+    echo "Please cd to the script's directory and try again."
+    exit 1
+fi
+
+# Check if the script is running with sudo privileges
 if [ "$EUID" -ne 0 ]; then
-    echo "Please run this script with: sudo ./install.sh" 2>&1
-    exit 1 
+    # Check if the user is in the sudo group
+    if groups $USER | grep &>/dev/null '\bsudo\b'; then
+        echo "You are in the sudo group, but didn't run this script with sudo privileges."
+        echo "Please run the script again using: sudo $0"
+    else
+        echo "You are not in the sudo group."
+        echo "Please add your username to the sudoers group and try again."
+        echo "You can do this by running: sudo usermod -aG sudo $USER"
+    fi
+    exit 1
 fi
 
 sudo xdotool getactivewindow windowsize 100% 100%
-
 sudo cp images/jgd_debian_avatar.png /home/$USER/.face
 sudo cp images/jgd_debian_avatar.png /home/$USER/.face.icon
 sudo mkdir -p /usr/share/backgrounds
 sudo cp images/jgd_debian_wallpaper.png /usr/share/backgrounds/
-sudo dconf write /org/gnome/desktop/background/picture-uri "'file:///usr/share/backgrounds/jgd_debian_wallpaper.png'"
 
 # This script should be run after 01_dependencies.sh
 # Check if dependencies from 01_dependencies.sh are installed.
@@ -84,11 +97,11 @@ echo "* App Name to Remove             * Source Repository                  *"
 echo "***********************************************************************"
 echo "* Firefox ESR                    * apt                                *"
 echo "***********************************************************************"
-echo ()
-echo ()
+echo ""
+echo ""
 echo "Installation will begin in:"
 echo "Type 's' to begin immediately, or wait for the countdown."
-for i in {20..1}
+for i in {45..1}
 do
    echo -ne "\r$i "
    read -t 1 -n 1 input
@@ -100,31 +113,24 @@ done
 
 if [[ $i -eq 0 ]]; then
    echo -e "\rStarting installation..."
+   sleep (3)
 fi
 
 sudo apt install blender -y
-
 sudo flatpak install flathub com.google.Chrome -y 
-
 sudo apt install color-picker -y
-
 sudo apt install curl -y
-
 sudo flatpak install flathub ca.desrt.dconf-editor -y
-
 sudo flatpak install flathub com.discordapp.Discord -y
 
 # Dropbox
 wget https://linux.dropboxstatic.com/packages/debian/dropbox_2024.04.17_amd64.deb
 sudo dpkg -i dropbox_2024.04.17_amd64.deb
-sudo rm -f dropbox_2024.04.17_amd64.deb -y
+sudo rm -f dropbox_2024.04.17_amd64.deb
 
 sudo apt install filezilla -y
-
 sudo apt install git -y
-
 sudo apt install gimp -y
-
 sudo flatpak install flathub org.kde.kdenlive -y
 
 # Moneydance
@@ -133,23 +139,19 @@ sudo dpkg -i moneydance_linux_amd64.deb
 sudo rm -i moneydance_linux_amd64.deb -y
 
 sudo flatpak install flathub md.obsidian.Obsidian -y
-    # Note that any obsidian plugins you already have installed will be synced on vault open. Restart Obsidian after they all load, and all works.
-
 sudo flatpak install flathub io.github.peazip.PeaZip -y
-
 sudo flatpak install flathub org.remmina.Remmina -y
-
-sudo curl -sS https://starship.rs/install.sh | sudo sh -y
-
+sudo curl -sS https://starship.rs/install.sh | sudo sh
 sudo flatpak install flathub org.mozilla.Thunderbird -y
-
-sudo apt install vlc -y
+sudo apt install vlc vlc-plugin-fluidsynth vlc-plugin-jack vlc-plugin-pipewire vlc-plugin-svg libdvdcss2 -y
+sudo dpkg-reconfigure libdvd-pkg
 
 # VSCode
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
 rm -f packages.microsoft.gpg
+sudo apt update
 sudo apt install code
 sudo rm -f ./code_*_amd64.deb
 code --install-extension sourcegraph.cody-ai
@@ -171,10 +173,9 @@ wget https://raw.githubusercontent.com/justrajdeep/fonts/master/Wingdings%203.tt
 wget https://raw.githubusercontent.com/justrajdeep/fonts/master/Webdings.ttf
 wget https://raw.githubusercontent.com/justrajdeep/fonts/master/Symbol.ttf
 wget https://raw.githubusercontent.com/dv-anomaly/ttf-wps-fonts/master/mtextra.ttf
-sudo cp /./home/jeremy/Downloads/*.ttf /usr/share/fonts/wps-office
+sudo *.ttf /usr/share/fonts/wps-office
 cd /home/jeremy/Downloads
 sudo rm -f *.ttf
-cd ~
 
 # Nerd Fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip 
@@ -182,13 +183,12 @@ sudo unzip JetBrainsMono.zip -d/usr/share/fonts
 sudo rm -f JetBrainsMono.zip
 
 sudo flatpak install flathub org.gnome.NautilusPreviewer -y
-
 sudo apt remove firefox-esr -y
 
-# Post config cleanup
+ssh-keygen -t ed25519 -C "jeremy.g.davenport@gmail.com"
+
 sudo apt autoremove
 sleep 2
-
 clear
 
 echo "The following configuration step may require user intervention and input to complete."
@@ -197,32 +197,26 @@ while [ "$user_input" != "yes" ]; do
     read -p "Please enter 'yes' to continue: " user_input
 done
 
-# Generate SSH key
-ssh-keygen -t ed25519 -C "jeremy.g.davenport@gmail.com"
-
-echo "Configuration has been successfully completed."
-echo "A system snapshot will now be captured using Timeshift."
-sleep 2
-
 # Update font cache 
 sudo fc-cache -f -v
 gsettings set org.gnome.desktop.interface font-name 'JetBrainsMonoNerdFont-Regular 11'
-gsettings set org.gnome.desktop.interface monospace-font-name 'JetBrainsMonoNerdFont-ExtraLight 11'
 dconf reset -f /org/gnome/terminal/legacy/profiles:/
 PROFILE_ID=$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d "'")
 echo "Updating terminal font for profile with ID: $PROFILE_ID (default)"
 dconf write /org/gnome/terminal/legacy/profiles:/:$PROFILE_ID/font "'JetBrainsMono Nerd Font Mono 11'"
 dconf write /org/gnome/terminal/legacy/profiles:/:$PROFILE_ID/use-system-font false
-
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
-
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
 sudo mkdir -p ~/.config
 sudo cp starship.toml ~/.config/starship.toml
 
+echo "Configuration has been successfully completed."
+echo "A system snapshot will now be captured using Timeshift."
+sleep 2
+
 # Timeshift backup
+sudo timeshift --rsync 
 sudo timeshift --create --comments "Fresh install, customized" --tags D
 sleep 2
 clear
@@ -244,7 +238,7 @@ while true; do
 done
 
 # Post config reboot
-for i in {10..1}; do
+for i in {30..1}; do
     echo "Rebooting in $i seconds..."
     sleep 1
 done
