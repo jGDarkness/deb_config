ef6a42d13eee599c32efe867a3f0fcd7f0f2a0f7
jGDarkness <jeremy.g.davenport@gmail.com>
2024-07-24 14:03:21 -0400

    acticated backport kernel

--- 01_dependencies.sh
+++ 01_dependencies.sh
@@ -25,11 +25,11 @@ sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flat
 
 sudo apt install nvidia-driver firmware-misc-nonfree -y
 
-# Remove the lines with (: <<'END_COMMENT') and (END_COMMENT) to install the latest kernel, and ensure it's updated
+# Comment out the lines with (: <<'END_COMMENT') and (END_COMMENT) to install the latest kernel, and ensure it's updated
 # when the rest of the system updates. Replace them before and after the block without the () to install the stable
 # kernel.
 
-: <<'END_COMMENT'
+#: <<'END_COMMENT'
 sudo apt -t bookworm-backports install linux-image-amd64 linux-headers-amd64 -y
 sudo tee -a /etc/apt/preferences << EOF
 Package: linux-image-*
@@ -40,7 +40,7 @@ Package: linux-headers-*
 Pin: release a=bookworm-backports
 Pin-Priority: 500
 EOF
-END_COMMENT
+#END_COMMENT
 
 
 # Reboot
