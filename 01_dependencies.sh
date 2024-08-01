af6b4adc476be765c25066a2a639f4b3efba9855
jGDarkness <jeremy.g.davenport@gmail.com>
2024-08-01 11:58:26 -0400

    added cURL to dependencies

--- 01_dependencies.sh
+++ 01_dependencies.sh
@@ -32,7 +32,7 @@ sudo truncate -s 0 /etc/apt/sources.list
 sudo cp sources.list /etc/apt/sources.list.d/sources.list
 sudo apt update
 sudo apt upgrade -y
-sudo apt install wget gpg apt-transport-https flatpak gnome-software-plugin-flatpak timeshift linux-headers-amd64 xdotool imagemagick chrome-gnome-shell gnome-shell-extension-manager -y
+sudo apt install curl wget gpg apt-transport-https flatpak gnome-software-plugin-flatpak timeshift linux-headers-amd64 xdotool imagemagick chrome-gnome-shell gnome-shell-extension-manager -y
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 sudo apt install nvidia-driver firmware-misc-nonfree -y
 
